d7340c8623830902c28fc191102f316b8ff0228b
Tianyu Du <tianyudu@stanford.edu>
2022-03-02 17:54:01 -0800

    update plot code

--- performance_plot.py
+++ performance_plot.py
@@ -1,3 +1,6 @@
+"""
+Analyze the performance of different algorithms on different simulated dataset.
+"""
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
@@ -8,6 +11,7 @@ plt.style.use(['science'])
 def run_experiment(A, X, Nx, Na, num_sensors, dim) -> pd.DataFrame:
     """Get performance of differnet algorithms with given input data."""
     solver_performance_list = list()
+
     X_hat = SOCP(A, Nx, Na, num_sensors, dim)
     solver_performance_list.append({'solver': 'SOCP', 'mse': MSE(X, X_hat)})
 
@@ -17,43 +21,46 @@ def run_experiment(A, X, Nx, Na, num_sensors, dim) -> pd.DataFrame:
     X_hat = noisy_SDP(A, Nx, Na, num_sensors, dim)
     solver_performance_list.append({'solver': 'noisy SDP', 'mse': MSE(X, X_hat)})
 
-    X_hat = steepest_descent_with_projection(A, X, Nx, Na, num_sensors, dim, tol=1E-7)
+    X_hat = steepest_descent_with_projection(A, X, Nx, Na, num_sensors, dim, tol=1E-7, verbose=False)
     solver_performance_list.append({'solver': 'projection', 'mse': MSE(X, X_hat)})
 
     return pd.DataFrame(solver_performance_list)
 
 
 if __name__ == "__main__":
-    num_trials = 2
+    # configure the simulations.
+    num_trials = 10
     num_anchors = 3
     num_sensors = 10
+    threshold_range = [0.3, 0.5, 1.0, 1.5, 2.0, 3.0, 5.0, 8.0]
     dim = 2
 
-    #
+    # performance comparison among solvers with infinity threshold.
     lst = list()
     for _ in tqdm(range(num_trials)):
         A, X = simulate_anchor_and_sensor(num_anchors, num_sensors, dim)
         A = np.array([[-1, -1], [0, 1], [1 ,-1]])
-        Nx, Na = calculate_distance(A, X, np.inf)
+        Nx, Na = calculate_distance(A, X, threshold=np.inf)
         lst.append(run_experiment(A, X, Nx, Na, num_sensors, dim))
     df = pd.concat(lst)
+    df['log_mse'] = np.log(df['mse'])
+    df.to_csv('./out/mse_infinity_threshold.csv', index=False)
 
     plt.close()
     fig, ax = plt.subplots(figsize=(8, 6))
-    sns.boxplot(x='solver', y='mse', data=df, ax=ax)
-    ax.set_yscale('log')
-    fig.savefig('./out/temp.png')
+    sns.boxplot(x='solver', y='log_mse', data=df, ax=ax)
+    fig.savefig('./out/mse_infinity_threshold.png')
 
-    #
+    # comparsing performance
     lst = list()
     N_size = list()
     for _ in tqdm(range(num_trials), desc='Random Data'):
         A, X = simulate_anchor_and_sensor(num_anchors, num_sensors, dim)
         A = np.array([[-1, -1], [0, 1], [1 ,-1]])
-        for threshold in tqdm(np.linspace(0.1, 3, 5), leave=False, desc='Threshold'):
-            Nx, Na = calculate_distance(A, X, threshold)
+        for threshold in tqdm(threshold_range, leave=False, desc='Threshold'):
+            Nx, Na = calculate_distance(A, X, threshold=threshold)
             N_size.append({'threshold': threshold, '|Nx|': len(Nx), '|Na|': len(Na)})
-            if len(Nx) + len(Na) == 0:
+            if len(Nx) == 0 or len(Na) == 0:
                 continue
             out = run_experiment(A, X, Nx, Na, num_sensors, dim)
             out['threshold'] = threshold
@@ -79,6 +86,4 @@ if __name__ == "__main__":
     plt.close()
     fig, ax = plt.subplots(figsize=(8, 6))
     sns.lineplot(x='threshold', y='log_mse', hue='solver', ci='sd', data=df, ax=ax)
-    sns.despine()
-    # ax.set_yscale('log')
     fig.savefig('./out/mse_by_threshold.png')
