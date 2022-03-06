39ba4a4d1b03d53a959faf84bbcd487d2a745312
Tianyu Du <tianyudu@stanford.edu>
2022-03-05 21:45:57 -0800

    update

--- Q2.py
+++ Q2.py
@@ -32,16 +32,28 @@ if __name__ == '__main__':
             X_hat = algorithms.least_square(A, Nx, Na, num_sensors, dim, verbose=False, X_init=X_init)
             performance_list.append({'threshold': threshold, 'solver': 'LS_with_SOCP_initialization', 'MSE': MSE(X, X_hat)})
 
-    breakpoint()
     df = pd.DataFrame(performance_list).reset_index(drop=True)
     df['MSE'] = df['MSE'].astype(float)
     df['LOG_MSE'] = np.log(df['MSE'])
 
+    # save the result.
     df.to_csv('./out/Q2_performance.csv', index=False)
 
+    # compare the performance against normal LS.
+    df_q1 = pd.read_csv('./out/Q1_performance.csv')
+    df = pd.concat([df, df_q1], axis=0)
+    df = df.reset_index(drop=True)
+
+    df = df[df['solver'].isin(['LS', 'LS_with_SOCP_initialization'])]
     # plot the result.
-    mean = df.groupby('threshold')['LOG_MSE'].mean().round(3).astype(str)
-    std = df.groupby('threshold')['LOG_MSE'].std().round(2).astype(str)
+    mean = df.groupby(['threshold', 'solver'])['LOG_MSE'].mean().round(3).astype(str)
+    std = df.groupby(['threshold', 'solver'])['LOG_MSE'].std().round(2).astype(str)
 
     table = mean + '<pm>' + std
-    print(table.reset_index().T.to_latex().replace('<pm>', '$\\pm$'))
+    table = pd.pivot(table.reset_index(), 'solver', 'threshold')
+    print(table.to_latex().replace('<pm>', '$\\pm$'))
+
+    plt.close()
+    fig, ax = plt.subplots(figsize=(6, 4))
+    sns.lineplot(x='threshold', y='LOG_MSE', hue='solver', ci='sd', data=df, ax=ax)
+    fig.savefig('./out/Q2_comparison.png', dpi=300)
