25b09f2fdb8bbfa305c00968de5fe5fc63aab0c5
Tianyu Du <tianyudu@stanford.edu>
2022-03-07 00:20:09 -0800

    add ADMM algorithm

--- algorithms.py
+++ algorithms.py
@@ -235,3 +235,103 @@ def noisy_SDP(A: np.ndarray, Nx: dict, Na: dict, num_sensors: int, dim: int=2):
 
     X_hat = Z.value[-n:, :d]
     return X_hat
+
+
+def ADMM(A: np.ndarray, X_true: np.ndarray, Nx: dict, Na: dict, num_sensors: int, dim: int=2, verbose: bool=False, tol: float=1E-7):
+    beta = 30
+    # initialize
+    X = np.random.randn(num_sensors, dim)
+    Y = np.random.randn(num_sensors, dim)
+    Lambda = np.random.randn(num_sensors, dim)
+
+    num_anchors = A.shape[0]
+
+    MAX_EPOCHS = 1000
+    for _ in range(MAX_EPOCHS):
+        # ==============================================================================================================
+        # update X.
+        # ==============================================================================================================
+        Q_l_list = list()
+        v_l_list = list()
+        for l in range(num_sensors):
+            Q_l = np.zeros((dim, dim))
+            v_l = np.zeros((dim, 1))
+
+            for j in range(l, num_sensors):
+                if (l, j) in Nx.keys():
+                    d2 = Nx[(l, j)]
+                    yl, yj = Y[l, :].reshape(-1, 1), Y[j, :].reshape(-1, 1)
+                    xj = X[j, :].reshape(-1, 1)
+
+                    Q_l += 2 * (yl - yj) @ (yl - yj).T
+                    v_l += 2 * (xj.T @ (yl - yj) + d2).T * (yl - yj)
+
+            for k in range(num_anchors):
+                if (k, l) in Na.keys():
+                    d2 = Na[(k, l)]
+                    ak = A[k, :].reshape(-1, 1)
+                    yl = Y[l, :].reshape(-1 ,1)
+                    Q_l += 2 * (ak - yl) @ (ak - yl).T
+
+                    v_l += 2 * (ak.T @ (ak - yl) - d2).T * (ak - yl)
+
+            Q_l += beta * np.eye(dim)
+            v_l += (Lambda[l, :] + beta * Y[l, :]).reshape(dim, 1)
+
+            Q_l_list.append(Q_l)
+            v_l_list.append(v_l)
+
+
+        for l in range(num_sensors):
+            X[l] = (np.linalg.inv(Q_l_list[l]) @ v_l_list[l]).reshape(dim)
+
+        # ==============================================================================================================
+        # update Y.
+        # ==============================================================================================================
+        M_l_list = list()
+        u_l_list = list()
+        for l in range(num_sensors):
+            M_l = np.zeros((dim, dim))
+            u_l = np.zeros((dim, 1))
+
+            for j in range(l, num_sensors):
+                if (l, j) in Nx.keys():
+                    d2 = Nx[(l, j)]
+                    xl, xj = X[l, :].reshape(-1, 1), X[j, :].reshape(-1, 1)
+                    yj = Y[j, :].reshape(-1, 1)
+
+                    M_l += 2 * (xl - xj) @ (xl - xj).T
+                    u_l += 2 * (yj.T @ (xl - xj) + d2).T * (xl - xj)
+
+            for k in range(num_anchors):
+                if (k, l) in Na.keys():
+                    d2 = Na[(k, l)]
+                    ak = A[k, :].reshape(-1, 1)
+                    xl = X[l, :].reshape(-1 ,1)
+
+                    M_l += 2 * (ak - xl) @ (ak - xl).T
+                    u_l += 2 * (ak.T @ (ak - xl) - d2).T * (ak - xl)
+
+            M_l += beta * np.eye(dim)
+            u_l -= Lambda[l, :].reshape(dim, 1)
+            u_l += beta * X[l, :].reshape(dim, 1)
+
+            M_l_list.append(M_l)
+            u_l_list.append(u_l)
+
+        for l in range(num_sensors):
+            Y[l] = (np.linalg.inv(M_l_list[l]) @ u_l_list[l]).reshape(dim)
+
+        # ==============================================================================================================
+        # Update Lambda
+        # ==============================================================================================================
+        for l in range(num_sensors):
+            Lambda[l] -= beta * (X[l] - Y[l])
+
+        # ==============================================================================================================
+        # Report
+        # ==============================================================================================================
+        if verbose:
+            print(np.mean((X_true - X)**2))
+
+    return X
